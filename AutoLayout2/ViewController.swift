7537f49d39230e3bad57f95987551ce17d5ba5d3
Sunmonu-Adedeji Olawale <ssg315ass@yahoo.com>
2020-01-07 11:15:24 +0100

    Initial Commit

---
+++ AutoLayout2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoLayout2
+//
+//  Created by Wale on 07/01/2020.
+//  Copyright © 2020 Wale. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
